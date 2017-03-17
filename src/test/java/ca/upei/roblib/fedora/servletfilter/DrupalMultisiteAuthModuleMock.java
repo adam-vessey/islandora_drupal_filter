ff6b39d1cd6d8ecd920a64b8c8499a6b1adaadb1
Adam Vessey <adam-vessey@users.noreply.github.com>
2017-03-17 12:01:50 -0300

    Small fixes...

--- src/test/java/ca/upei/roblib/fedora/servletfilter/DrupalMultisiteAuthModuleMock.java
+++ src/test/java/ca/upei/roblib/fedora/servletfilter/DrupalMultisiteAuthModuleMock.java
@@ -1,30 +1,18 @@
 package ca.upei.roblib.fedora.servletfilter;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.Element;
 
 public class DrupalMultisiteAuthModuleMock extends DrupalMultisiteAuthModule {
-    protected String key;
-
-    public DrupalMultisiteAuthModuleMock(String key) throws IOException, DocumentException {
+    public DrupalMultisiteAuthModuleMock() throws IOException, DocumentException {
         super();
-        this.key = key;
     }
 
     @Override
     protected void parseConfig() throws DocumentException, IOException {
         Document doc = getParsedConfig(this.getClass().getResourceAsStream("/filter-drupal-multisite.xml"));
-
-        @SuppressWarnings("unchecked")
-        List<Element> list = doc.selectNodes("//FilterDrupal_Connection/connection");
-        for (Element el : list) {
-            config.put(key, parseConnectionElement(el));
-            // One is all we need.
-            break;
-        }
+        parseConfig(doc);
     }
 }
